24c42ca0c584044cedc903ea9096b98b06ed4a77
leocavalcante <lc@leocavalcante.com>
2020-05-31 15:38:55 -0300

    Running web components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {Injector, NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
+import {createCustomElement} from "@angular/elements";
 
 @NgModule({
   declarations: [
@@ -21,4 +22,10 @@ import {MatIconModule} from "@angular/material/icon";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+  constructor(private injector: Injector) {
+    const elem = createCustomElement(AppComponent, {injector});
+    customElements.define('micro-angular', elem);
+  }
+}
