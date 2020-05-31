b87e188915a2c1338b41038413127a89203247ab
leocavalcante <lc@leocavalcante.com>
2020-05-30 22:52:16 -0300

    We have the counter example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'micro-angular';
+  counter = 0;
+
+  increment() {
+    this.counter++;
+  }
+
+  decrement() {
+    this.counter--;
+  }
 }
